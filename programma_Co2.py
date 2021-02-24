06ec5e70178f945964ba7c1c47dde74aba256139
Agnese18 <78478660+Agnese18@users.noreply.github.com>
2021-02-24 09:59:01 +0200

    Update programma_Co2.py

--- programma_Co2.py
+++ programma_Co2.py
@@ -1,6 +1,5 @@
 x=int(input("ievadi 1, ja velies aprekinat tikai vienai automasinai CO2 izmaksas, vai ievadi 2 ja vēlies aprekinat 2 automasinam CO2 izmaksas - "))
-sum=0 
-for i in range (x): 
+sum=0  
     a=int(input("ievadi savas automasinas CO2 izmesu daudzumu-")) #tiks ievadits skatlis noteiktas reizes
     
 
